86c2b04ea3f1bdb66c702de83d7bff6c9929e3ee
Dave May <dave@davemay.org>
2015-11-09 09:09:27 -0700

    Added in coffeescript.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var coffee = require('gulp-coffee');
 
-gulp.task('scripts', function() {
-    gulp.src('src/js/*.js')
+gulp.task('coffee', function() {
+    return gulp.src('src/coffee/*.coffee')
+        .pipe(coffee())
+        .pipe(gulp.dest('src/js/'));
+});
+
+gulp.task('scripts', ['coffee'], function() {
+    return gulp.src('src/js/*.js')
         .pipe(concat('all.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
---
+++ src/coffee/third.coffee
@@ -0,0 +1,2 @@
+myThirdFunction = ->
+  "third"
